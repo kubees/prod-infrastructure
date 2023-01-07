d256dc4536c32f13cb0849d6a03d924b47f2213a
AhmedGrati <ahmedgrati1999@gmail.com>
2023-01-07 12:39:23 +0100

    extract workloads ns

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,3 +1,12 @@
+locals {
+  nginx_ingress_controller_ns = "ingress-nginx"
+  microservices_ns            = "microservices"
+  database_ns                 = "databases"
+  frontend_ns                 = "frontend"
+  argocd_ns                   = "automation"
+}
+
 module "nginx-ingress-controller" {
-  source = "nginx-ingress-controller"
+  source                      = "./nginx-ingress-controller"
+  nginx_ingress_controller_ns = local.nginx_ingress_controller_ns
 }
